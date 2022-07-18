dbf2455aa1523b826cd74b7786c4a3bdefd255a4
sameerauf1 <sameerauf1@gmail.com>
2022-07-17 21:25:26 -0700

    done

---
+++ summer_project/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h1> Hello World</h1>
+      <Product name = 'Amazon Echo' description = 'Your Ai Assistant' price ={599} />
+    </div>
+  );
+}
+
+export default App;
