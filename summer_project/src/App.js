256600226f30a96600976fa3f5c6e458c6b68f22
sameerauf1 <sameerauf1@gmail.com>
2022-07-18 12:57:49 -0700

    added todos sucessfuly

--- summer_project/src/App.js
+++ summer_project/src/App.js
@@ -1,10 +1,27 @@
-import React from "react";
-import styles from "./App.css";
+import React, { useState } from "react";
+//import styles from "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (e) => {
+    //console.log(object)
+    setTodos([...todos, input]);
+  };
   return (
     <div className="app">
-      <h1> Hello World</h1>
+      <h1> Welcome to my TODO List</h1>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        type="text"
+      />
+      <button onClick={addTodo}> Add to Do </button>
+      <h2> List of Todos</h2>
+      {todos.map((todo) => {
+        return <p> {todo}</p>;
+      })}
     </div>
   );
 }
