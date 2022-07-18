e5d7bb261a985ee3be69b5325b06e03e255e1053
sameerauf1 <sameerauf1@gmail.com>
2022-07-17 22:43:59 -0700

    yes

--- summer_project/src/product.js
+++ summer_project/src/product.js
@@ -4,10 +4,10 @@ import styles from "./Product.modules.css";
 
 function Product({ name, description, price }) {
   return (
-    <div className="product">
+    <div className={styles.product}>
       <ItemDescription name={name} description={description} />
-      <h2 className="product__error"> An error occured</h2>
-      <h3 className="product__price"> ${price}</h3>
+      <h2 className={styles.error}> An error occured</h2>
+      <h3 className={styles.price}> ${price}</h3>
     </div>
   );
 }
