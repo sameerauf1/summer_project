bfabe9591c970dfaf70359b6aa9116a3d53aa572
sameerauf1 <sameerauf1@gmail.com>
2022-07-18 12:15:28 -0700

    getting rid of cs modules

--- summer_project/src/App.js
+++ summer_project/src/App.js
@@ -1,27 +1,10 @@
 import React from "react";
-import Product from "./Product";
-import styles from "./App.module.css";
+import styles from "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <h1> Hello World</h1>
-      <h2 className={styles.error}> An Error has occured </h2>
-      <Product
-        name="Google Home"
-        description="Your Ai Assistant"
-        price={59.99}
-      />
-      <Product
-        name="Alexa"
-        description="The assistant that always listends"
-        price={159.99}
-      />
-      <Product
-        name="Siri"
-        description="The all knowing apple assistant"
-        price={19.99}
-      />
     </div>
   );
 }
